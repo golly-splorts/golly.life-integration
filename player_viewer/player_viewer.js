97accfed0f66c4629cf9bbd2e41d144fa37bb341
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2020-12-22 21:34:20 -0800

    Automatic deploy of integration at 2020-12-23-05-34-20

--- player_viewer/player_viewer.js
+++ player_viewer/player_viewer.js
@@ -498,7 +498,7 @@
         // Draw cells
         for (i = 0 ; i < PlayerViewer.columns; i++) {
           for (j = 0 ; j < PlayerViewer.rows; j++) {
-            if ((i==PlayerViewer.activeRow) && (j==PlayerViewer.activeCol)) {
+            if ((j==PlayerViewer.activeRow) && (i==PlayerViewer.activeCol)) {
               this.drawCell(i, j, true);
             } else {
               this.drawCell(i, j, false);

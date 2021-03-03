a69fb2fa8812340f46ef800c935bbd58bd305dea
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-03-02 18:45:56 -0800

    Automatic deploy of integration at 2021-03-03-02-45-56

--- maps.js
+++ maps.js
@@ -12,6 +12,8 @@
       'container-maps'
     ],
 
+    season0 : 0,
+
     init : function() {
       this.loading();
       this.populateMapCards();
@@ -46,110 +48,144 @@
      */
     populateMapCards : function() {
 
-      var mapRowElem = document.getElementById('row-maps');
-
-      let mapsUrl = this.baseApiUrl + '/maps';
-      fetch(mapsUrl)
+      // get current day/season info from API /today
+      let url = this.baseApiUrl + '/today';
+      fetch(url)
       .then(res => res.json())
-      .then((mapsApiResult) => {
+      .then((todayApiResult) => {
 
-        this.loadingElem.classList.add('invisible');
-        var mapsContainer = document.getElementById('container-maps');
-        mapsContainer.classList.remove('invisible');
+        if (todayApiResult[0]==-1) {
+          this.season0 = 1;
+        } else {
+          this.season0 = todayApiResult[0];
+        }
 
-        var iM;
-        for (iM = 0; iM < mapsApiResult.length; iM++) {
-          thisMap = mapsApiResult[iM];
+        var mapRowElem = document.getElementById('row-maps');
 
-          // Create a clone of the map card template
-          var mapCardTemplate = document.getElementById('map-card-template');
-          var cloneFragment = mapCardTemplate.content.cloneNode(true);
+        let mapsUrl = this.baseApiUrl + '/maps';
+        fetch(mapsUrl)
+        .then(res => res.json())
+        .then((mapsApiResult) => {
 
-          // Add the map pattern name as the id
-          if (thisMap.hasOwnProperty('patternName')) {
-            cloneFragment.querySelector(".card").setAttribute("id", thisMap.patternName);
-          }
-
-          // Add the template map card to the page
-          mapRowElem.appendChild(cloneFragment);
-
-          // Get a reference to the card we just added
-          var mapCard = document.getElementById(thisMap.patternName);
-
-          // Set the title of the map card to the map name
-          var mapCardTitleElems = mapCard.getElementsByClassName('map-card-map-name');
-          var iE;
-          for (iE = 0; iE < mapCardTitleElems.length; iE++) {
-            var elem = mapCardTitleElems[iE];
-            elem.innerHTML = thisMap.mapName;
-          }
-
-          // Set img src
-          var imgElems = mapCard.getElementsByClassName('map-card-image');
-          var ii;
-          for (ii = 0; ii < imgElems.length; ii++) {
-            var imgElem = imgElems[ii];
-            imgElem.setAttribute('src', this.baseUIUrl + '/img/' + thisMap.patternName + '.png');
-          }
-
-          // Set map description
-          var descElems = mapCard.getElementsByClassName('map-card-description');
-          var iD;
-          for (iD = 0; iD < descElems.length; iD++) {
-            var descElem = descElems[iD];
-            if (thisMap.hasOwnProperty('mapDescription')) {
-              descElem.innerHTML = thisMap.mapDescription;
-            } else {
-              descElem.innerHTML = "(No description found)";
-            }
-          }
-
-          // Add the NEW label to any map newer than Season 10
-          var newBadgeElems = mapCard.getElementsByClassName('map-badge-new');
-          var iNew;
-          for (iNew = 0; iNew < newBadgeElems.length; iNew++) {
-            var newBadgeElem = newBadgeElems[iNew];
-            if (thisMap.hasOwnProperty('mapSeason')) {
-              if (thisMap.mapSeason>=10) {
-                newBadgeElem.classList.remove('invisible');
-              } else {
-                newBadgeElem.remove();
-              }
-            }
-          }
-
-          // Add the season label to all maps
-          var seaBadgeElems = mapCard.getElementsByClassName('map-badge-season');
-          var iSea;
-          for (iSea = 0; iSea < seaBadgeElems.length; iSea++) {
-            var seaBadgeElem = seaBadgeElems[iSea];
-            if (thisMap.hasOwnProperty('mapSeason')) {
-              seaBadgeElem.innerHTML = "Season " + (thisMap.mapSeason+1);
-              seaBadgeElem.classList.remove('invisible');
-            }
-          }
-
-          // set inner html of map season badge, and make it visible
+          this.loadingElem.classList.add('invisible');
           var mapsContainer = document.getElementById('container-maps');
           mapsContainer.classList.remove('invisible');
 
-          // Set simulate button
-          var simElems = mapCard.getElementsByClassName('simulate');
-          var iS;
-          for (iS = 0; iS < simElems.length; iS++) {
-            var simElem = simElems[iS];
+          var iM;
+          for (iM = 0; iM < mapsApiResult.length; iM++) {
+            thisMap = mapsApiResult[iM];
+
+            // Create a clone of the map card template
+            var mapCardTemplate = document.getElementById('map-card-template');
+            var cloneFragment = mapCardTemplate.content.cloneNode(true);
+
+            // Add the map pattern name as the id
             if (thisMap.hasOwnProperty('patternName')) {
-              simElem.setAttribute('href', this.baseUIUrl + '/simulator/index.html?patternName=' + thisMap.patternName);
+              cloneFragment.querySelector(".card").setAttribute("id", thisMap.patternName);
             }
+
+            // Add the template map card to the page
+            mapRowElem.appendChild(cloneFragment);
+
+            // Get a reference to the card we just added
+            var mapCard = document.getElementById(thisMap.patternName);
+
+            // Set the title of the map card to the map name
+            var mapCardTitleElems = mapCard.getElementsByClassName('map-card-map-name');
+            var iE;
+            for (iE = 0; iE < mapCardTitleElems.length; iE++) {
+              var elem = mapCardTitleElems[iE];
+              elem.innerHTML = thisMap.mapName;
+            }
+
+            // Set img src
+            var imgElems = mapCard.getElementsByClassName('map-card-image');
+            var ii;
+            for (ii = 0; ii < imgElems.length; ii++) {
+              var imgElem = imgElems[ii];
+              imgElem.setAttribute('src', this.baseUIUrl + '/img/' + thisMap.patternName + '.png');
+            }
+
+            // Set map description
+            var descElems = mapCard.getElementsByClassName('map-card-description');
+            var iD;
+            for (iD = 0; iD < descElems.length; iD++) {
+              var descElem = descElems[iD];
+              if (thisMap.hasOwnProperty('mapDescription')) {
+                descElem.innerHTML = thisMap.mapDescription;
+              } else {
+                descElem.innerHTML = "(No description found)";
+              }
+            }
+
+            // Make new label visible if map start date within 2 seasons
+            var newBadgeElems = mapCard.getElementsByClassName('map-badge-new');
+            var iNew;
+            for (iNew = 0; iNew < newBadgeElems.length; iNew++) {
+              var newBadgeElem = newBadgeElems[iNew];
+              if (thisMap.hasOwnProperty('mapStartSeason')) {
+                console.log(thisMap.mapStartSeason - this.season0);
+                if ((this.season0 - thisMap.mapStartSeason) <= 2) {
+                  newBadgeElem.classList.remove('invisible');
+                } else {
+                  newBadgeElem.remove();
+                }
+              }
+            }
+
+            // Make retired label visible if map end date >= current season
+            var retBadgeElems = mapCard.getElementsByClassName('map-badge-retired');
+            var iRet;
+            for (iRet = 0; iRet < retBadgeElems.length; iRet++) {
+              var retBadgeElem = retBadgeElems[iRet];
+              if (thisMap.hasOwnProperty('mapEndSeason')) {
+                if (thisMap.mapEndSeason <= this.season0) {
+                  retBadgeElem.classList.remove('invisible');
+                } else {
+                  retBadgeElem.remove();
+                }
+              }
+            }
+
+            // Add the season label to all maps
+            var seaBadgeElems = mapCard.getElementsByClassName('map-badge-season');
+            var iSea;
+            for (iSea = 0; iSea < seaBadgeElems.length; iSea++) {
+              var seaBadgeElem = seaBadgeElems[iSea];
+              if (thisMap.hasOwnProperty('mapStartSeason')) {
+                seaBadgeElem.innerHTML = "Season " + (thisMap.mapStartSeason+1);
+                seaBadgeElem.classList.remove('invisible');
+              }
+            }
+
+            // set inner html of map season badge, and make it visible
+            var mapsContainer = document.getElementById('container-maps');
+            mapsContainer.classList.remove('invisible');
+
+            // Set simulate button
+            var simElems = mapCard.getElementsByClassName('simulate');
+            var iS;
+            for (iS = 0; iS < simElems.length; iS++) {
+              var simElem = simElems[iS];
+              if (thisMap.hasOwnProperty('patternName')) {
+                simElem.setAttribute('href', this.baseUIUrl + '/simulator/index.html?patternName=' + thisMap.patternName);
+              }
+            }
+
           }
 
-        }
+        })
+        .catch(err => {
+          console.log(err);
+          this.error(-1);
+        }); // end API /maps
 
       })
       .catch(err => {
         console.log(err);
         this.error(-1);
-      }); // end API /maps
+      });
+
     },
 
     /**
